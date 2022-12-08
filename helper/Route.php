27733ac031bba4a4a9786cc00a79aeee4765124b
gpisano97 <49675849+gpisano97@users.noreply.github.com>
2022-12-08 19:23:06 +0100

    Preflight CORS
    Added the function enableCORS of Route Class for enable preflight CORS request.

--- helper/Route.php
+++ helper/Route.php
@@ -114,4 +114,15 @@ class Route {
         include($file);
         exit();
     }
+    
+    public function enableCORS($allowedOrigin = "*"){
+        if($_SERVER["REQUEST_METHOD"] === "OPTIONS"){
+            http_response_code(204);
+            header("Access-Control-Allow-Origin: {$allowedOrigin}");
+            header("Access-Control-Allow-Method: POST, GET, OPTIONS, PUT, DELETE");
+            header("Content-Type: application/json");
+            header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, Origin");
+            exit;
+        }
+    }
 }
